18e4fd8b4adbc257bb1fcf55105eaf2abc8082b7
iancinti <ian.cinti@gmail.com>
2024-04-24 00:28:03 -0300

    Initial commit

---
+++ src/main/kotlin/com/cclabsf/msthreads/MsThreadsApplication.kt
@@ -0,0 +1,11 @@
+package com.cclabsf.msthreads
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MsThreadsApplication
+
+fun main(args: Array<String>) {
+	runApplication<MsThreadsApplication>(*args)
+}
---
+++ src/test/kotlin/com/cclabsf/msthreads/MsThreadsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.cclabsf.msthreads
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MsThreadsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
